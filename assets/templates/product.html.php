415262405ba6660fd952ed1a5b71e096c285b93a
Corey McMahon <contact@coreymcmahon.com>
2013-10-14 18:29:17 +0700

    Improve layout and styles for product template

--- assets/templates/product.html.php
+++ assets/templates/product.html.php
@@ -7,38 +7,31 @@
         <?php endif; ?>
 
         <?php if (in_array('image', $fields)): ?>
-            <div class="wp-bc-product-image">
-                <img src="<?php echo "{$store_url}/product_images/{$product->image->image_file}"; ?>" 
-                     alt="<?php echo $product->name; ?>" 
-                     <?php 
+        <div class="wp-bc-product-image">
+            <img src="<?php echo "{$store_url}/product_images/{$product->image->image_file}"; ?>" 
+                 alt="<?php echo $product->name; ?>" 
+                 <?php 
 
-                     if (!empty($image_width)) {
-                        echo "width=\"{$image_width}\" ";
-                     }
-                     if (!empty($image_height)) {
-                        echo "height=\"{$image_height}\" ";
-                     }
+                 if (!empty($image_width)) {
+                    echo "width=\"{$image_width}\" ";
+                 }
+                 if (!empty($image_height)) {
+                    echo "height=\"{$image_height}\" ";
+                 }
 
-                     echo "style=\"";
-                     if (!empty($image_width)) echo "width: {$image_width}px;";
-                     if (!empty($image_height)) echo "height: {$image_height}px;";
-                     echo "\"";
+                 echo "style=\"";
+                 if (!empty($image_width)) echo "width: {$image_width}px;";
+                 if (!empty($image_height)) echo "height: {$image_height}px;";
+                 echo "\"";
 
-                     ?>
-                />
-            </div>
+                 ?>
+            />
+        </div>
         <?php endif; ?>
 
+        <div class="wp-bc-fields">
         <?php if (in_array('sku', $fields)): ?>
             <div class="wp-bc-product-sku"><?php echo $product->sku; ?></div>
-        <?php endif; ?>
-
-        <?php if (in_array('description', $fields)): ?>
-            <div class="wp-bc-product-description">
-                <?php $description = substr(preg_replace('/<[^<]+?>/', ' ', $product->description), 0, 255); ?>
-                <?php echo $description ?>
-                <?php if (strlen($description) === 255) echo '&hellip;'; ?>
-            </div>
         <?php endif; ?>
 
         <?php if (in_array('description-html', $fields)): ?>
@@ -47,7 +40,7 @@
 
         <?php if (in_array('price', $fields)): ?>
             <div class="wp-bc-product-price">
-                <div class="wp-bc-label">Price</div>
+                <div class="wp-bc-label">Price:</div>
                 <div class="wp-bc-value">
                     <?php echo $store->currency_symbol; ?>
                     <?php echo number_format(
@@ -62,70 +55,70 @@
 
         <?php if (in_array('condition', $fields)): ?>
             <div class="wp-bc-product-condition">
-                <div class="wp-bc-label">Condition</div>
+                <div class="wp-bc-label">Condition:</div>
                 <div class="wp-bc-value"><?php echo $product->condition; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('warranty', $fields)): ?>
             <div class="wp-bc-product-warranty">
-                <div class="wp-bc-label">Warranty</div>
+                <div class="wp-bc-label">Warranty:</div>
                 <div class="wp-bc-value"><?php echo $product->warranty; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('inventory', $fields)): ?>
             <div class="wp-bc-product-inventory">
-                <div class="wp-bc-label">Inventory level</div>
+                <div class="wp-bc-label">Inventory level:</div>
                 <div class="wp-bc-value"><?php echo $product->inventory_level; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('weight', $fields)): ?>
             <div class="wp-bc-product-weight">
-                <div class="wp-bc-label">Weight</div>
+                <div class="wp-bc-label">Weight:</div>
                 <div class="wp-bc-value"><?php echo $product->weight; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('width', $fields)): ?>
             <div class="wp-bc-product-width">
-                <div class="wp-bc-label">Width</div>
+                <div class="wp-bc-label">Width:</div>
                 <div class="wp-bc-value"><?php echo $product->width; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('height', $fields)): ?>
             <div class="wp-bc-product-height">
-                <div class="wp-bc-label">Height</div>
+                <div class="wp-bc-label">Height:</div>
                 <div class="wp-bc-value"><?php echo $product->height; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('depth', $fields)): ?>
             <div class="wp-bc-product-depth">
-                <div class="wp-bc-label">Depth</div>
+                <div class="wp-bc-label">Depth:</div>
                 <div class="wp-bc-value"><?php echo $product->depth; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('rating', $fields)): ?>
             <div class="wp-bc-product-rating">
-                <div class="wp-bc-label">Rating</div>
+                <div class="wp-bc-label">Rating:</div>
                 <div class="wp-bc-value"><?php echo $product->rating; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('rating-total', $fields)): ?>
             <div class="wp-bc-product-rating-total">
-                <div class="wp-bc-label">Rating total</div>
+                <div class="wp-bc-label">Rating total:</div>
                 <div class="wp-bc-value"><?php echo $product->rating_total; ?></div>
             </div>
         <?php endif; ?>
 
         <?php if (in_array('rating-count', $fields)): ?>
             <div class="wp-bc-product-rating-rating-count">
-                <div class="wp-bc-label">Rating count</div>
+                <div class="wp-bc-label">Rating count:</div>
                 <div class="wp-bc-value"><?php echo $product->rating_count; ?></div>
             </div>
         <?php endif; ?>
@@ -133,7 +126,7 @@
         <!-- @TODO: fix this
         <?php if (in_array('brand', $fields)): ?>
             <div class="wp-bc-product-brand">
-            <div class="wp-bc-label">Brand</div>
+            <div class="wp-bc-label">Brand:</div>
             <div class="wp-bc-value"><?php print_r($product->brand, false); ?></div>
         </div>
         <?php endif; ?>
@@ -145,6 +138,16 @@
                 <div class="wp-bc-product-category-<?php echo $category->id; ?>">
                 </div>
                 <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+
+        </div>
+
+        <?php if (in_array('description', $fields)): ?>
+            <div class="wp-bc-product-description">
+                <?php $description = substr(preg_replace('/<[^<]+?>/', ' ', $product->description), 0, 255); ?>
+                <?php echo $description ?>
+                <?php if (strlen($description) === 255) echo '&hellip;'; ?>
             </div>
         <?php endif; ?>
 
